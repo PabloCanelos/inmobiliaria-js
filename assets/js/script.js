1c801a3f36f1b7121e9435b24f091391b2e01c3f
Pablo Canelos <pabloccanelos.contacto@gmail.com>
2024-11-29 17:21:02 -0300

    codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
-const carta = document.querySelector('.navbar-brand')
-console.log()
+const alquilerProperties = [
+    {
+        title: "Apartamento en el centro de la ciudad",
+        location: "123 Main Street, Anytown, CA 91234",
+        price: 2000,
+        bedrooms: 2,
+        bathrooms: 2,
+        petsAllowed: true,
+        smokingAllowed: false,
+        imageUrl: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+    },
+    {
+        title: "Apartamento luminoso con vista al mar",
+        location: "456 Ocean Avenue, Seaside Town, CA 56789",
+        price: 2500,
+        bedrooms: 3,
+        bathrooms: 3,
+        petsAllowed: true,
+        smokingAllowed: true,
+        imageUrl: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        title: "Condominio moderno en zona residencial",
+        location: "789 Lakeview Street, Anytown, CA 12345",
+        price: 2200,
+        bedrooms: 2,
+        bathrooms: 2,
+        petsAllowed: false,
+        smokingAllowed: true,
+        imageUrl: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+    },
+    {
+        title: "Casa amplia en las afueras",
+        location: "123 Suburban Lane, Suburbia, CA 98765",
+        price: 3000,
+        bedrooms: 4,
+        bathrooms: 3,
+        petsAllowed: true,
+        smokingAllowed: false,
+        imageUrl: "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+    }
+];
